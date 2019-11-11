df1e99354eb1a48f43fae88e1d549bb976c6940f
USER <USER@FACE-97>
2019-11-11 18:11:10 +0530

    completed

---
+++ encapsulation/src/case4/ticket21.java
@@ -0,0 +1,40 @@
+package case4;
+
+public class ticket21 {
+int ticket;
+int p;
+String name;
+String gender;
+int age;
+public int getTicket() {
+	return ticket;
+}
+public void setTicket(int ticket) {
+	this.ticket = ticket;
+}
+public int getP() {
+	return p;
+}
+public void setP(int p) {
+	this.p = p;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+
+}
